43a949beb5ff40f637b1568a2078b966504f2727
Felipe A. Moreno <fmorenovr@gmail.com>
2020-03-27 00:36:50 -0500

    Renaming

--- Readme.md
+++ Readme.md
@@ -1,18 +1,18 @@
 # Computer Science - UCSP - Arequipa - Perú
 
-Cursos de Maestría en Ciencia de la Computacion por el [Centro de Investigación e Innovación en Ciencia de la Computación (RICS)](http://rics.ucsp.edu.pe/) en el [Programa de Maestría en Ciencia de la Computación](http://rics.ucsp.edu.pe/mcs/index.html) de la [Universidad Católica San Pablo (UCSP)](http://ucsp.edu.pe/) desde el 2018 hasta el 2020.
+Cursos de Maestría en Ciencia de la Computación por el [Centro de Investigación e Innovación en Ciencia de la Computación (RICS)](http://rics.ucsp.edu.pe/) en el [Programa de Maestría en Ciencia de la Computación](http://rics.ucsp.edu.pe/mcs/index.html) de la [Universidad Católica San Pablo (UCSP)](http://ucsp.edu.pe/) desde el 2018 hasta el 2020.
 
 ## Periodo 2018-I:
 
     MCC101 - Algoritmos y Estructura de Datos
-    MCC102 - Matematica para la Computacion
-    MCC103 - Programacion Paralela y Distribuida
+    MCC102 - Matematica Computacional
+    MCC103 - Computación Paralela y Distribuida
 
 ## Periodo 2018-II:
 
-    MCC201 - Imagenes
-    MCC202 - Sistemas Inteligentes
-    MCC203 - Graficos
+    MCC201 - Procesamiento de Imágenes y Visión Computacional
+    MCC202 - Inteligencia Artificial
+    MCC203 - Computación Gráfica
 
 ## Periodo 2019-I:
 
---
+++ calculate_GPA.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# encoding: utf-8
+
+def getNota(nota):
+  if 17<=nota and nota<=20:
+    return 4.0, "A"
+  elif 14<=nota and nota<17:
+    return 3.0, "B"
+  elif 12<=nota and nota<14:
+    return 2.0, "C"
+  else:
+    return 1.0, "D"
+
+def getGPA(courses_list, depurar=False):
+  total_credits = 0.0
+  ponderate_credits = 0.0
+  ponderate_grades = 0.0
+  for name_couse, credit, grade in courses_list:
+    points, letter = getNota(grade)
+    if depurar and letter=="D":
+      continue    
+    total_credits += credit
+    ponderate_credits += credit*points
+    ponderate_grades += credit*grade
+    #print(credit, points, grade, total_credits, ponderate_credits)
+  
+  return ponderate_credits/total_credits, ponderate_grades/total_credits
+
+
+courses_list = [
+# 2018-I
+("Lineal", 4, 15.63), ("EDA", 4, 13.66), ("Paralelas", 4, 15.73), ("Seminario de Tesis I", 4, 20),
+# 2018-II
+("Imágenes", 4, 12.75), ("IA", 4, 14.02), ("Gráficos", 4, 14.60), ("Seminario de Tesis II", 4, 19),
+# 2019-I
+("Seminarios Avanzados de Computación", 4, 15), ("Seminario de Tesis III", 12, 19),
+# 2019-II
+("Seminario de Tesis IV", 16, 19),
+]
+
+GPA, AVG = getGPA(courses_list, False)
+
+print("No Depurado GPA: ", GPA, "Average: ", AVG)
+
+GPA, AVG = getGPA(courses_list, True)
+
+print("Depurado GPA: ", GPA, "Average: ", AVG)
